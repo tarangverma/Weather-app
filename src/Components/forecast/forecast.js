68dd7485e9cb1511811ced8eb3fe9f0d08fc0a0e
Tarang Verma <tarangverma004@gmail.com>
2022-11-11 21:42:52 +0530

    Final commit
    Signed-off-by: Tarang Verma <tarangverma004@gmail.com>

--- src/Components/forecast/forecast.js
+++ src/Components/forecast/forecast.js
@@ -32,7 +32,34 @@ export const Forecast = ({data}) => {
                               </div>
                           </AccordionItemButton>
                        </AccordionItemHeading>
-                       <AccordionItemPanel></AccordionItemPanel>
+                       <AccordionItemPanel>
+                          <div className='daily-details-grid'>
+                            <div className='daily-details-grid-item'>
+                                <label>Pressure</label>
+                                <label>{item.main.pressure} Pa</label>
+                            </div>
+                            <div className='daily-details-grid-item'>    
+                                <label>Humidity</label>
+                                <label>{item.main.humidity}%</label>
+                            </div>
+                            <div className='daily-details-grid-item'>    
+                                <label>Clouds</label>
+                                <label>{item.clouds.all}</label>
+                            </div>
+                            <div className='daily-details-grid-item'>    
+                                <label>Wind Speed</label>
+                                <label>{item.wind.speed} m/s</label>
+                            </div>
+                            <div className='daily-details-grid-item'>    
+                                <label>Sea level</label>
+                                <label>{item.main.sea_level}</label>
+                            </div>
+                            <div className='daily-details-grid-item'>    
+                                <label>feels like</label>
+                                <label>{Math.round(item.main.feels_like)}°C</label>
+                            </div>
+                          </div>
+                       </AccordionItemPanel>
                 </AccordionItem>
             ))}
                
